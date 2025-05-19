1161df4c3661c9ecf51c1250e38e170821df2d05
Emil <emilaqvist@gmail.com>
2025-05-19 19:51:03 +0200

    Fixes
    Fixes with initiative, UI, feed, top bar, profile...

---
+++ src/main/java/com/localzero/api/controller/IndexController.java
@@ -0,0 +1,11 @@
+package com.localzero.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/") public String toDefaultPath(){
+        return "feed";
+    }
+}
---
+++ src/main/java/com/localzero/api/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.localzero.api.controller;
+
+import com.localzero.api.entity.Person;
+import com.localzero.api.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private PersonService ps;
+
+    @GetMapping("/profile")
+    public String showProfile(Model model, @AuthenticationPrincipal UserDetails currentUser){
+        if(currentUser == null){
+            return "redirect:/login";
+        }
+
+        Person person = ps.findByEmail(currentUser.getUsername());
+        if (person == null){
+            return "redirect:/login";
+        }
+
+        model.addAttribute("user",person);
+        return "profile";
+    }
+
+
+}
